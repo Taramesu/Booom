f397a0796a735cca60e39a3a37718bcce7fad59b
Taramesu <zhongxhouxiang@outlook.com>
2023-10-03 17:41:03 +0800

    reactor: 重写房间生成机制 feat: 完成房间通道生成功能

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -1,21 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class test : MonoBehaviour
 {
     public Transform spawnPoint;
     public GameObject roomPrefab;
+    public GameObject doorPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         RoomGenerator.Instance.roomPrefabName = "Room1";
         RoomGenerator.Instance.roomQuantity = 7;
+        RoomGenerator.Instance.doorPrefabs = doorPrefab;
 
         RoomGenerator.Instance.generatorPoint = spawnPoint;
-        RoomGenerator.Instance.xOffset = 2;
-        RoomGenerator.Instance.yOffset = 2;
+        RoomGenerator.Instance.xOffset = 40;
+        RoomGenerator.Instance.yOffset = 20;
         RoomGenerator.Instance.roomLayer = LayerMask.GetMask("Room");
     }
 
@@ -28,6 +31,11 @@ public class test : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.J))
         {
             RoomGenerator.Instance.GenerateRooms();
+        }
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
